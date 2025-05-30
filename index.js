de1bbd8ffbfa9246258b4a23f0c8211e2c81bac1
BM-ACADEMY <teamkamarweb@gmail.com>
2025-05-30 18:10:37 +0530

    cors setup

--- index.js
+++ index.js
@@ -11,7 +11,25 @@ dotenv.config();
 // App setup
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: [process.env.FRONTEND_URL, process.env.PRODUCTION_URL],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}))
+
+app.use((req, res, next) => {
+    const allowedOrigins = [process.env.FRONTEND_URL, process.env.PRODUCTION_URL];
+    const origin = req.headers.origin;
+    
+    if (allowedOrigins.includes(origin)) {
+        res.header("Access-Control-Allow-Origin", origin);
+    }
+
+    res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH, OPTIONS");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    next();
+});
 
 // Routes
 const ContactForm = require('./route/contactFormRoute');
